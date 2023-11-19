714d139a74b4e7b7a7c17c7ac2bfa7aed8f3a6bb
GiorgiJulakidze1997 <g.julakidze1997@gmail.com>
2023-11-19 23:20:49 +0400

    delete comments and smth.

--- src/App.jsx
+++ src/App.jsx
@@ -14,8 +14,7 @@ function App() {
   return (
     <div>
       <Header />
-      <p>hello</p>
-    {/* hello */}
+    
       <Routes>
         <Route path='/' element={<HomePage />} />
         <Route path='complex' element={<Complex />} />
