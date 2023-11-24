0b844e2384a15521cf612e46968285c6130d2ea1
Guram12 <guram.shanidze.33@gmail.com>
2023-11-25 00:39:57 +0400

    test

--- src/pages/Complex.jsx
+++ src/pages/Complex.jsx
@@ -81,6 +81,7 @@ export default function Complex({favoriteHandler}) {
       {/* ეს არის ჩამონათვალი button–ები, რომ გადახვიდე კომპლექსებზე, გეგმარებებზე, რუკაზე, სორტირება... */}
       <div className='infoFieldOfComplexsPlansMaps'>
         <p>კომპლექსები {totalCount}</p>
+        <p>test</p>
         <div className='projectsPlansMapsBox'>
           <Link to='/complex' ><button>პროექტები</button></Link>
           <Link to='/complex/plans' ><button>გეგმარებები</button></Link>
