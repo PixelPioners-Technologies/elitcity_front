883f411435c9178615630fb1a3d2bd1590b2c5ca
georgetoloraia <gtoloraia19@gmail.com>
2024-01-06 19:16:18 +0400

    main pageze - gaketda -- fasi -- farti -- lokacia -- mdebareoba -- statusi jer kidev dasamatebelia bevri ram - [x] rikis linkze gadasvla

---
+++ src/Components/HomePage/HomePageFilterModal/SpaceModal.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const SpaceModal = () => {
+  // Implement your SpaceModal component here
+  return (
+    <div>
+      space
+      {/* Your SpaceModal content */}
+    </div>
+  );
+};
+
+export default SpaceModal;
